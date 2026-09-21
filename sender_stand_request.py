dc85d59538cfb1de168a1b369a01c9fc81920545
aakosits-prog <aakosits@gmail.com>
2026-09-21 11:36:26 +0300

    Добавлены комментарии к тесту

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,14 +2,18 @@ import requests
 import configuration
 import data
 
+# Функция для отправки POST-запроса на создание нового заказа
 def post_new_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
                          json=body,
                          headers=data.headers)
+
+# Функция для создания заказа и извлечения его трек-номера
 def get_order_track():
     response = post_new_order(data.order_body)
     return response.json()["track"]
 
+# Функция для отправки GET-запроса и получения данных заказа по треку
 def get_order_by_track(track):
     params = {"t": track}
     response = requests.get(
--- test_avto.py
+++ test_avto.py
@@ -1,6 +1,7 @@
 import sender_stand_request
 import data
 
+# Тест проверяет, что код ответа равен 200 при получении заказа по треку
 def test_positive_assert():
     track = sender_stand_request.get_order_track()
     response = sender_stand_request.get_order_by_track(track)
